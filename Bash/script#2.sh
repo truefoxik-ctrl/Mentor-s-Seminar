b330b2a75062a8aa799e98b7bb5e280ac11df3a7
TATIANA MAKARKINA <true.foxik@gmail.com>
2025-11-01 22:13:01 +0300

    Update script#2.sh

--- Bash/script#2.sh
+++ Bash/script#2.sh
@@ -1,3 +1,4 @@
+#Напишите скрипт, который принимает два числа и выводит информацию o том, какое из них больше или меньше, либо о том, что числа равны
 if [ $1 -gt $2 ]; then echo "$1 is greater"
 elif [ $1 -lt $2 ]; then echo "$1 is smaller"
 elif [ $1 -eq $2 ]; then echo "they are equal"
