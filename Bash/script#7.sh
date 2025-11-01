0d04d3520ffc766c12e9117e4483e49e17d881f7
TATIANA MAKARKINA <true.foxik@gmail.com>
2025-11-01 23:10:14 +0300

    Update script#7.sh

--- Bash/script#7.sh
+++ Bash/script#7.sh
@@ -1,1 +1,1 @@
-curl -s -X POST "https://api.telegram.org/bot8472294023:AAEB9ncfyAWrHSuRnuq-emFaoR1NCp5EqAo/sendMessage" -d chat_id="1383959005" -d text="Сегодня 1 ноября. На улице +2 градуса"
+curl -s -X POST "https://api.telegram.org/bot////sendMessage" -d chat_id="1383959005" -d text="Сегодня 1 ноября. На улице +2 градуса"
